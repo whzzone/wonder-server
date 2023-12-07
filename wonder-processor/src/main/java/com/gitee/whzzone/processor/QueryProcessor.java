98a32c4316a77f888113188442752516773e5977
whz <whz@live.com>
2023-12-07 09:49:59 +0800

    [feat] 优化

--- wonder-processor/src/main/java/com/gitee/whzzone/processor/QueryProcessor.java
+++ wonder-processor/src/main/java/com/gitee/whzzone/processor/QueryProcessor.java
@@ -40,57 +40,57 @@ public class QueryProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         messager.printMessage(Diagnostic.Kind.NOTE, "------开始校验 @Query 注解------");
-        for (TypeElement annotation : annotations) {
-            for (VariableElement fieldElement : ElementFilter.fieldsIn(roundEnv.getElementsAnnotatedWith(annotation))) {
-                Query queryAnnotation = fieldElement.getAnnotation(Query.class);
-                String column = queryAnnotation.column();
+        for (VariableElement fieldElement : ElementFilter.fieldsIn(roundEnv.getElementsAnnotatedWith(Query.class))) {
+            Query queryAnnotation = fieldElement.getAnnotation(Query.class);
+            String column = queryAnnotation.column();
 
-                boolean isBetween = queryAnnotation.expression() == ExpressionEnum.BETWEEN;
-                if (isBetween && column.isEmpty()) {
+            if (queryAnnotation.expression() != ExpressionEnum.BETWEEN) {
+                continue;
+            }
+
+            if (column.isEmpty()) {
+                messager.printMessage(Diagnostic.Kind.ERROR,
+                        "@Query注解'expression'为'BETWEEN'时，'column'不能为空：" + fieldElement.getSimpleName(), fieldElement);
+            }
+
+            String className = ((TypeElement) fieldElement.getEnclosingElement()).getQualifiedName().toString();
+            //TODO 20231122 待处理BETWEEN的类型 Integer 和 Date 不能比较
+            TypeMirror type = fieldElement.asType();
+            messager.printMessage(Diagnostic.Kind.WARNING, type.toString());
+
+            String key = className + "#" + column;
+            if (columnCountMap.containsKey(key)) {
+                int count = columnCountMap.get(key) + 1;
+                if (count > 2) {
                     messager.printMessage(Diagnostic.Kind.ERROR,
-                            "@Query注解'expression'为'BETWEEN'时，'column'不能为空：" + fieldElement.getSimpleName(), fieldElement);
+                            "@Query注解'expression'为'BETWEEN'时，'column'相同的必须成对使用：" + fieldElement.getSimpleName() + "，未为成对的column：" + column, fieldElement);
+                } else {
+                    columnCountMap.put(key, count);
+                }
+            } else {
+                columnCountMap.put(key, 1);
+            }
+
+            // 查找同类中相同column,且expression() == ExpressionEnum.BETWEEN
+            boolean foundMatch = false;
+            for (VariableElement otherFieldElement : ElementFilter.fieldsIn(elementUtils.getAllMembers((TypeElement) fieldElement.getEnclosingElement()))) {
+                if (otherFieldElement == fieldElement) {
                     continue;
                 }
-
-                String className = ((TypeElement) fieldElement.getEnclosingElement()).getQualifiedName().toString();
-                //TODO 20231122 待处理BETWEEN的类型 Integer 和 Date 不能比较
-                TypeMirror type = fieldElement.asType();
-                messager.printMessage(Diagnostic.Kind.WARNING, type.toString());
-
-                String key = className + "#" + column;
-                if (isBetween && columnCountMap.containsKey(key)) {
-                    int count = columnCountMap.get(key) + 1;
-                    if (count > 2) {
-                        messager.printMessage(Diagnostic.Kind.ERROR,
-                                "@Query注解'expression'为'BETWEEN'时，'column'相同的必须成对使用：" + fieldElement.getSimpleName() + "，未为成对的column：" + column, fieldElement);
-                    } else {
-                        columnCountMap.put(key, count);
-                    }
-                } else {
-                    columnCountMap.put(key, 1);
+                Query otherQueryAnnotation = otherFieldElement.getAnnotation(Query.class);
+                if (otherQueryAnnotation != null
+                        && otherQueryAnnotation.column().equals(column)
+                        && otherQueryAnnotation.expression() == ExpressionEnum.BETWEEN
+                ) {
+                    foundMatch = true;
+                    break;
                 }
+            }
 
-                // 查找相同column但左右不同的属性
-                boolean foundMatch = false;
-                for (VariableElement otherFieldElement : ElementFilter.fieldsIn(elementUtils.getAllMembers((TypeElement) fieldElement.getEnclosingElement()))) {
-                    if (otherFieldElement == fieldElement) {
-                        continue;
-                    }
-                    Query otherQueryAnnotation = otherFieldElement.getAnnotation(Query.class);
-                    if (otherQueryAnnotation != null
-                            && otherQueryAnnotation.column().equals(column)
-                            && otherQueryAnnotation.expression() == ExpressionEnum.BETWEEN
-                    ) {
-                        foundMatch = true;
-                        break;
-                    }
-                }
-
-                // 如果无法找到匹配的属性，则报错
-                if (isBetween && !foundMatch) {
-                    messager.printMessage(Diagnostic.Kind.ERROR,
-                            "@Query注解'expression'为'BETWEEN'时，'column'必须成对存在：" + column, fieldElement);
-                }
+            // 如果无法找到匹配的属性，则报错
+            if (!foundMatch) {
+                messager.printMessage(Diagnostic.Kind.ERROR,
+                        "@Query注解'expression'为'BETWEEN'时，'column'必须成对存在：" + column, fieldElement);
             }
         }
         messager.printMessage(Diagnostic.Kind.NOTE, "------处理 @Query 注解结束------");
