59424277b78522b302c12857af491a09bd46ee3e
httpsjt <362055143@qq.com>
2024-01-26 16:57:15 +0800

    工具类优化。

--- wonder-common/src/main/java/com/gitee/whzzone/common/util/CommonUtil.java
+++ wonder-common/src/main/java/com/gitee/whzzone/common/util/CommonUtil.java
@@ -1,44 +1,41 @@
 package com.gitee.whzzone.common.util;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Create by whz at 2024/1/24
  */
 public class CommonUtil {
 
-    public static Field[] getAllFieldsIncludingParents(Class<?> clazz) {
+    public static Field[] getAllFieldsIncludingParents(Class<?> cls) {
+        Objects.requireNonNull(cls, "cls");
         List<Field> allFields = new ArrayList<>();
 
-        // 获取当前类的所有属性（包括私有属性）
-        Field[] declaredFields = clazz.getDeclaredFields();
-        for (Field field : declaredFields) {
-            allFields.add(field);
-        }
+        // 获取当前类的所有属性
+        Field[] currentFields = cls.getDeclaredFields();
+
+        Arrays.stream(currentFields).filter(field -> !Modifier.isStatic(field.getModifiers()))
+            .forEach(v -> allFields.add(v));
 
         // 获取父类的所有属性
-        Class<?> superClass = clazz.getSuperclass();
-        if (superClass != null) {
-            Field[] parentFields = getAllFieldsIncludingParents(superClass);
-            for (Field field : parentFields) {
-                // 避免重复添加相同的属性
-                if (!containsFieldWithName(allFields, field.getName())) {
-                    allFields.add(field);
+        for (Class<?> parentClass = cls.getSuperclass(); parentClass != null;
+            parentClass = parentClass.getSuperclass()) {
+            Field[] parentFields = parentClass.getDeclaredFields();
+
+            // 避免重复添加相同的属性 及去除 静态属性如 :serialVersionUID
+            for (Field parentField : parentFields) {
+                String name = parentField.getName();
+                if (!allFields.stream().anyMatch(v -> v.getName().equalsIgnoreCase(name))
+                    && !Modifier.isStatic(parentField.getModifiers())) {
+                    allFields.add(parentField);
                 }
             }
         }
-
         return allFields.toArray(new Field[0]);
-    }
-
-    private static boolean containsFieldWithName(List<Field> fields, String fieldName) {
-        for (Field field : fields) {
-            if (field.getName().equals(fieldName)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
